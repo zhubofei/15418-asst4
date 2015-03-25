296e0397cb37351a2502609b63eb6e6dc63a7f9d
Bofei Zhu <zhu.bofei@gmail.com>
2015-03-25 12:31:33 -0400

    finish compareprimes

--- src/myserver/worker.cpp
+++ src/myserver/worker.cpp
@@ -17,65 +17,18 @@
 WorkQueue<Request_msg> work_queue;
 WorkQueue<Request_msg> tell_queue;
 
-// Generate a valid 'countprimes' request dictionary from integer 'n'
-static void create_computeprimes_req(Request_msg& req, int n) {
-  std::ostringstream oss;
-  oss << n;
-  req.set_arg("cmd", "countprimes");
-  req.set_arg("n", oss.str());
-}
-
-// Implements logic required by compareprimes command via multiple
-// calls to execute_work.  This function fills in the appropriate
-// response.
-static void execute_compareprimes(const Request_msg& req, Response_msg& resp) {
-
-    int params[4];
-    int counts[4];
-
-    // grab the four arguments defining the two ranges
-    params[0] = atoi(req.get_arg("n1").c_str());
-    params[1] = atoi(req.get_arg("n2").c_str());
-    params[2] = atoi(req.get_arg("n3").c_str());
-    params[3] = atoi(req.get_arg("n4").c_str());
-
-    for (int i=0; i<4; i++) {
-      Request_msg dummy_req(0);
-      Response_msg dummy_resp(0);
-      create_computeprimes_req(dummy_req, params[i]);
-      execute_work(dummy_req, dummy_resp);
-      counts[i] = atoi(dummy_resp.get_response().c_str());
-    }
-
-    if (counts[1]-counts[0] > counts[3]-counts[2])
-      resp.set_response("There are more primes in first range.");
-    else
-      resp.set_response("There are more primes in second range.");
-}
-
 // For the use of pthread
-void* worker_thread(void* arg) {
+static void* worker_thread(void* arg) {
    while (1) {
      Request_msg req = work_queue.get_work();
      Response_msg resp(req.get_tag());
-
-     if (req.get_arg("cmd").compare("compareprimes") == 0) {
-       // The compareprimes command needs to be special cased since it is
-       // built on four calls to execute_execute work.  All other
-       // requests from the client are one-to-one with calls to
-       // execute_work.
-       execute_compareprimes(req, resp);
-     } else {
-       // actually perform the work.  The response string is filled in by
-       // 'execute_work'
-       execute_work(req, resp);
-     }
+     execute_work(req, resp);
      worker_send_response(resp);
    }
    return NULL;
 }
 
-void* tell_thread(void* arg) {
+static void* tell_thread(void* arg) {
    while (1) {
      Request_msg req = tell_queue.get_work();
      Response_msg resp(req.get_tag());
