89e90bce446267a228568294a6c4ebdd04dac4d9
Bofei Zhu <zhu.bofei@gmail.com>
2015-03-24 21:21:56 -0400

    support tellmenow

--- src/myserver/worker.cpp
+++ src/myserver/worker.cpp
@@ -12,9 +12,10 @@
 #include "tools/cycle_timer.h"
 #include "tools/work_queue.h"
 
-#define MAX_THREADS_NUM 24
+#define MAX_THREADS_NUM 23 // leave one for tellmenow
 
 WorkQueue<Request_msg> work_queue;
+WorkQueue<Request_msg> tell_queue;
 
 // Generate a valid 'countprimes' request dictionary from integer 'n'
 static void create_computeprimes_req(Request_msg& req, int n) {
@@ -55,20 +56,30 @@ static void execute_compareprimes(const Request_msg& req, Response_msg& resp) {
 // For the use of pthread
 void* worker_thread(void* arg) {
    while (1) {
-     Request_msg my_req = work_queue.get_work();
-     Response_msg resp(my_req.get_tag());
+     Request_msg req = work_queue.get_work();
+     Response_msg resp(req.get_tag());
 
-     if (my_req.get_arg("cmd").compare("compareprimes") == 0) {
+     if (req.get_arg("cmd").compare("compareprimes") == 0) {
        // The compareprimes command needs to be special cased since it is
        // built on four calls to execute_execute work.  All other
        // requests from the client are one-to-one with calls to
        // execute_work.
-       execute_compareprimes(my_req, resp);
+       execute_compareprimes(req, resp);
      } else {
        // actually perform the work.  The response string is filled in by
        // 'execute_work'
-       execute_work(my_req, resp);
+       execute_work(req, resp);
      }
+     worker_send_response(resp);
+   }
+   return NULL;
+}
+
+void* tell_thread(void* arg) {
+   while (1) {
+     Request_msg req = tell_queue.get_work();
+     Response_msg resp(req.get_tag());
+     execute_work(req, resp);
      worker_send_response(resp);
    }
    return NULL;
@@ -88,7 +99,8 @@ void worker_node_init(const Request_msg& params) {
     pthread_t t;
     pthread_create(&t, NULL, &worker_thread, NULL);
   }
-
+  pthread_t t;
+  pthread_create(&t, NULL, &tell_thread, NULL);
 }
 
 void worker_handle_request(const Request_msg& req) {
@@ -97,6 +109,9 @@ void worker_handle_request(const Request_msg& req) {
   // configuration, this would be in the log logs/worker.INFO)
   DLOG(INFO) << "Worker got request: [" << req.get_tag() << ":" << req.get_request_string() << "]\n";
 
-  // Add new works
-  work_queue.put_work(req);
+  if (req.get_arg("cmd").compare("tellmenow") == 0) {
+    tell_queue.put_work(req);
+  } else {
+    work_queue.put_work(req);
+  }
 }
